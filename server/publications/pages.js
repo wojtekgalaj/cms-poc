4f4298f34841303af6f957051a8984f9dd352907
Wojtek G <wojtek.galaj@gmail.com>
2016-02-09 16:55:03 +0100

    Add headers to api responses

--- server/publications/pages.js
+++ server/publications/pages.js
@@ -2,12 +2,20 @@ import {Pages} from '/libs/collections';
 import {Meteor} from 'meteor/meteor';
 import {check} from 'meteor/check';
 
+JsonRoutes.setResponseHeaders({
+  'Cache-Control': 'no-store',
+  Pragma: 'no-cache',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With'
+});
+
 Meteor.publish('page', function (title) {
   check(title, String);
   const selector = {title};
   return Pages.find(selector);
 }, {
-  url: '/page/:0',
+  url: '/api/page/:0',
   method: 'get'
 });
 
